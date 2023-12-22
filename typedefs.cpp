f0ffb1332db70160a081d3b15df7bb467b31a1be
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-22 17:43:13 +0300

    Update typedefs.cpp

--- typedefs.cpp
+++ typedefs.cpp
@@ -1,30 +1,29 @@
 #include <ratio>
-#include "nutility.h"
 #include <iostream>
 
-template<intmax_t x, intmax_t y>
-std::ostream& operator<<(std::ostream& os, std::ratio<x, y> r)
+template<intmax_t NUM, intmax_t DEN>
+std::ostream& operator<<(std::ostream& os, std::ratio<NUM, Den> r)
 {
-	return os << de(x) << " / " << de(y);
+	return os << NUM << " / " << DEN;
 }
 
 int main()
 {
 	using namespace std;
-	cout << "peta   = " << peta{} << "\n";
-	cout << "exa    = " << exa{} << "\n";
-	cout << "tera   = " << tera{} << "\n";
-	cout << "giga   = " << giga{} << "\n";
-	cout << "mega   = " << mega{} << "\n";
-	cout << "kilo   = " << kilo{} << "\n";
-	cout << "hecto  = " << hecto{} << "\n";
-	cout << "deca   = " << deca{} << "\n";
-	cout << "deci   = " << deci{} << "\n";
-	cout << "centi  = " << centi{} << "\n";
-	cout << "milli  = " << milli{} << "\n";
-	cout << "micro  = " << micro{} << "\n";
-	cout << "nano   = " << nano{} << "\n";
-	cout << "pico   = " << pico{} << "\n";
-	cout << "femto  = " << femto{} << "\n";
-	cout << "atto   = " << atto{} << "\n";
+	cout << "peta   = " << peta{} << '\n';
+	cout << "exa    = " << exa{} << '\n';
+	cout << "tera   = " << tera{} << '\n';
+	cout << "giga   = " << giga{} << '\n';
+	cout << "mega   = " << mega{} << '\n';
+	cout << "kilo   = " << kilo{} << '\n';
+	cout << "hecto  = " << hecto{} << '\n';
+	cout << "deca   = " << deca{} << '\n';
+	cout << "deci   = " << deci{} << '\n';
+	cout << "centi  = " << centi{} << '\n';
+	cout << "milli  = " << milli{} << '\n';
+	cout << "micro  = " << micro{} << '\n';
+	cout << "nano   = " << nano{} << '\n';
+	cout << "pico   = " << pico{} << '\n';
+	cout << "femto  = " << femto{} << '\n';
+	cout << "atto   = " << atto{} << '\n';
 }
