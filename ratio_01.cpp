e5e99895cb0089e6a0ff2440e136016e12abf25b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2025-01-22 16:54:07 +0300

    Update ratio_01.cpp

--- ratio_01.cpp
+++ ratio_01.cpp
@@ -2,13 +2,13 @@
 
 int main()
 {
-	using namespace std;
+	using std::ratio;
 
 	using two_third = ratio<2, 3>;
 	using one_fifth = ratio<1, 5>;
 
-	using ratio_result = ratio_add<two_third, one_fifth>;
-	
+	using ratio_result = std::ratio_add<two_third, one_fifth>;
+
 	constexpr auto num = ratio_result::num;
 	constexpr auto den = ratio_result::den;
 }
